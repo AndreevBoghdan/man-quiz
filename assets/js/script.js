5b0a441bd98042379aef5ad1f9f106cc9ead4908
Andreev Boghdan <boghdanandreev@gmail.com>
2017-02-15 12:46:35 +0200

    fix mousedown

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,3 @@
-    $('.next-button__not-last').click(function(e){
-        e.preventDefault();
-        $(this).parent().hide();
-        $(this).parent().next().show();
-
-        hideStat();
-        });
 
     $('.next-button__not-last').bind('touchstart', function(e){
         e.preventDefault();
@@ -14,15 +7,13 @@
         hideStat();
         });
 
-
-    $('.back-button__not-first').click(function(e){
+    $('.next-button__not-last').mousedown(function(e){
         e.preventDefault();
         $(this).parent().hide();
-        $(this).parent().prev().show();
-
-        hideStat()
-
+        $(this).parent().next().show();
+        hideStat();
         });
+
 
     $('.back-button__not-first').bind('touchstart', function(e){
         e.preventDefault();
@@ -33,24 +24,33 @@
 
         });
 
-
-    $('.back-button__first').click(function(e){
+    $('.back-button__not-first').mousedown(function(e){
         e.preventDefault();
-        window.external.PlayPreviousLcInChannelByName ('main');
+        $(this).parent().hide();
+        $(this).parent().prev().show();
+
+        hideStat()
+
         });
+
 
     $('.back-button__first').bind('touchstart',function(e){
         e.preventDefault();
         window.external.PlayPreviousLcInChannelByName ('main');
         });
 
+    $('.back-button__first').mousedown(function(e){
+        e.preventDefault();
+        window.external.PlayPreviousLcInChannelByName ('main');
+        });
 
-    $('.next-button__last').click(function(e){
+
+    $('.next-button__last').bind('touchstart', function(e){
         e.preventDefault();
         window.external.playNextContentStartsNameInChannel('umfrage', 'main');
         });
 
-    $('.next-button__last').bind('touchstart', function(e){
+    $('.next-button__last').mousedown(function(e){
         e.preventDefault();
         window.external.playNextContentStartsNameInChannel('umfrage', 'main');
         });
@@ -61,48 +61,6 @@
     }
 
 
-    $('.answer').click(function(e){
-        e.preventDefault();
-
-        $(this).parent().parent().addClass('question-answered');
-        var chosen = $(this);
-        var question = $(this).parent().parent().attr('id');
-        var questionNumber = $('#'+ question).attr('number');
-        $('#'+ question).attr('number', parseInt(questionNumber) + 1);
-        $('.question-answered').find('.answer').each(function( index ) {
-            var answer = $(this).attr('value');
-            $('#' + question +'-answer').attr("value",answer);
-            var answerNumber = $(this).attr('number');
-
-            if ($(this).attr('value')==chosen.attr('value')){
-                var ajaxUrl = "http://h1475695.stratoserver.net" +  $('#' + question + '_answer-' + answer + '-url').val();
-
-
-                var csrftoken = jQuery("[name=csrfmiddlewaretoken]").val();
-                $.ajax({
-                    type: "POST",
-                    url: ajaxUrl,
-                    success: statChangedSecceed,
-                    error:  function(resp){
-                        console.log(resp.responseText);
-                    },
-                    beforeSend: function(xhr, settings) {
-                            xhr.setRequestHeader("X-CSRFToken", csrftoken);
-                    }
-                    });
-                $(this).attr('number', parseInt(answerNumber)+1);
-                rate = (parseInt(answerNumber)+1) * 100 / (parseInt(questionNumber)+1);
-                $('#' + question + '_answer-' + answer + '-span').css("color", "red");
-            } else {
-                rate = (parseInt(answerNumber)) * 100 / (parseInt(questionNumber)+1);
-            }
-            rate = rate.toFixed(2)
-            $('#' + question + '_answer-' + answer + '-progress-span').text(rate + " %");
-            $('#' + question + '_answer-' + answer + '-progress').width(rate + "%");
-        });
-        showStat();
-
-        });
 
     $('.answer').bind('touchstart', function(e){
         e.preventDefault();
@@ -145,6 +103,46 @@
 
         });
 
+    $('.answer').mousedown(function(e){
+        e.preventDefault();
+
+        $(this).parent().parent().addClass('question-answered');
+        var chosen = $(this);
+        var question = $(this).parent().parent().attr('id');
+        var questionNumber = $('#'+ question).attr('number');
+        $('#'+ question).attr('number', parseInt(questionNumber) + 1);
+        $('.question-answered').find('.answer').each(function( index ) {
+            var answer = $(this).attr('value');
+            $('#' + question +'-answer').attr("value",answer);
+            var answerNumber = $(this).attr('number');
+
+            if ($(this).attr('value')==chosen.attr('value')){
+                var ajaxUrl = "http://h1475695.stratoserver.net" + $('#' + question + '_answer-' + answer + '-url').val();
+                var csrftoken = jQuery("[name=csrfmiddlewaretoken]").val();
+                $.ajax({
+                    type: "POST",
+                    url: ajaxUrl,
+                    success: statChangedSecceed,
+                    error:  function(resp){
+                        console.log(resp.responseText);
+                    },
+                    beforeSend: function(xhr, settings) {
+                        xhr.setRequestHeader("X-CSRFToken", csrftoken);
+                    }
+                    });
+                $(this).attr('number', parseInt(answerNumber)+1);
+                rate = (parseInt(answerNumber)+1) * 100 / (parseInt(questionNumber)+1);
+                $('#' + question + '_answer-' + answer + '-span').css("color", "red");
+            } else {
+                rate = (parseInt(answerNumber)) * 100 / (parseInt(questionNumber)+1);
+            }
+            rate = rate.toFixed(2)
+            $('#' + question + '_answer-' + answer + '-progress-span').text(rate + " %");
+            $('#' + question + '_answer-' + answer + '-progress').width(rate + "%");
+        });
+        showStat();
+
+        });
 
 showStat = function (){
     $('.question-answered').last().find('.answer').hide();
@@ -174,7 +172,7 @@ timeout = window.setTimeout(function(){
 }, 30000);
 
 
-$('body').click(function(){
+$('body').bind('touchstart', function(){
     window.clearTimeout(timeout);
     timeout = window.setTimeout(function(){
         playNextContentStartsNameInChannel("umfrage", 'main');
@@ -182,7 +180,7 @@ $('body').click(function(){
 
 });
 
-$('body').bind('touchstart', function(){
+$('body').mousedown(function(){
     window.clearTimeout(timeout);
     timeout = window.setTimeout(function(){
         playNextContentStartsNameInChannel("umfrage", 'main');
