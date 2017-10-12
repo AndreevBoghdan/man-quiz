c3dd28b6f5eef937c0dc15d4921948acfe904031
Andreev Boghdan <boghdanandreev@gmail.com>
2017-10-12 17:36:08 +0300

    wsgi path

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,7 @@
 import os
 import sys
 # Add the app's directory to the PYTHONPATH
-sys.path.append('/var/www/quiz')
+sys.path.append('~/httpdocs/quiz')
 os.environ['DJANGO_SETTINGS_MODULE'] = 'quiz.settings'
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
