2570ade24570b32ebcf4a428b2d00007a8c78572
Andreev Boghdan <boghdanandreev@gmail.com>
2017-10-13 11:07:30 +0300

    refresh wsgy.py

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,8 @@
 import os
 import sys
+import site
 # Add the app's directory to the PYTHONPATH
+site.addsitedir('~/httpdocs/quiz/.quizenv/local/lib/python2.7/site-packages')
 sys.path.append('~/httpdocs/quiz')
 os.environ['DJANGO_SETTINGS_MODULE'] = 'quiz.settings'
 from django.core.wsgi import get_wsgi_application
